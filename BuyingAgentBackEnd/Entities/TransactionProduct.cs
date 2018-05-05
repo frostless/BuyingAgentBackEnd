e7b9262712f3aa532933c1dc5823a3860ba7430b
wei zheng <tobe.zheng.wei@gmail.com>
2018-05-05 22:52:09 +1000

    fixed tab size

--- BuyingAgentBackEnd/Entities/TransactionProduct.cs
+++ BuyingAgentBackEnd/Entities/TransactionProduct.cs
@@ -2,13 +2,13 @@
 
 namespace BuyingAgentBackEnd.Entities
 {
-    public class TransactionProduct
-    {
-        public int ProductId { get; set; }
-        public Product Product { get; set; }
+	public class TransactionProduct
+	{
+		public int ProductId { get; set; }
+		public Product Product { get; set; }
 
-        public int TransactionId { get; set; }
-        public Transaction Transaction { get; set; }
+		public int TransactionId { get; set; }
+		public Transaction Transaction { get; set; }
 
 		[Required]
 		public int Qty { get; set; }
