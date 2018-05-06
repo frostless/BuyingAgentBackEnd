a242c72b7c79f177ad7595e38195174ab90b19b8
wei zheng <tobe.zheng.wei@gmail.com>
2018-05-06 17:00:21 +1000

    indent fix

--- BuyingAgentBackEnd/Controllers/ReportsController.cs
+++ BuyingAgentBackEnd/Controllers/ReportsController.cs
@@ -7,80 +7,80 @@ using System;
 
 namespace BuyingAgentBackEnd.Controllers
 {
-    [Route("api/reports")]
-    public class ReportsController : Controller
-    {
-        private IBuyingAgentRepository _buyingAgentRepository;
-        private ILogger<AddNewController> _logger;
+	[Route("api/reports")]
+	public class ReportsController : Controller
+	{
+		private IBuyingAgentRepository _buyingAgentRepository;
+		private ILogger<AddNewController> _logger;
 
-        public ReportsController(IBuyingAgentRepository buyingAgentRepository,
-            ILogger<AddNewController> logger)
-        {
-            _buyingAgentRepository = buyingAgentRepository;
-            _logger = logger;
-        }
+		public ReportsController(IBuyingAgentRepository buyingAgentRepository,
+			ILogger<AddNewController> logger)
+		{
+			_buyingAgentRepository = buyingAgentRepository;
+			_logger = logger;
+		}
 
-        [HttpGet("monthsProfit/{Id?}")]
-        public IActionResult monthsProfit(int Id)
-        {
-            if (Id == 0) Id = DateTime.Now.Year;
-            if (!_buyingAgentRepository.IfMonthsProfitExit(Id)) return NotFound();
-            var monthsProfit = _buyingAgentRepository.GetMonthsProfit(Id);
-            return Ok(monthsProfit);
-        }
+		[HttpGet("monthsProfit/{Id?}")]
+		public IActionResult monthsProfit(int Id)
+		{
+			if (Id == 0) Id = DateTime.Now.Year;
+			if (!_buyingAgentRepository.IfMonthsProfitExit(Id)) return NotFound();
+			var monthsProfit = _buyingAgentRepository.GetMonthsProfit(Id);
+			return Ok(monthsProfit);
+		}
 
-        [HttpGet("topCustomer")]
-        public IActionResult topCustomer()
-        {
-            var topCustomer = _buyingAgentRepository.GetTopCustomer();
-            return Ok(topCustomer);
-        }
+		[HttpGet("topCustomer")]
+		public IActionResult topCustomer()
+		{
+			var topCustomer = _buyingAgentRepository.GetTopCustomer();
+			return Ok(topCustomer);
+		}
 
-        [HttpGet("topProduct")]
-        public IActionResult topProduct()
-        {
-           var topProduct = _buyingAgentRepository.GetTopProduct();
-            return Ok(topProduct);
-        }
+		[HttpGet("topProduct")]
+		public IActionResult topProduct()
+		{
+			var topProduct = _buyingAgentRepository.GetTopProduct();
+			return Ok(topProduct);
+		}
 
-        [HttpGet("topPost")]
-        public IActionResult topPost()
-        {
-            var topPost = _buyingAgentRepository.GetTopPost();
-            return Ok(topPost);
-        }
+		[HttpGet("topPost")]
+		public IActionResult topPost()
+		{
+			var topPost = _buyingAgentRepository.GetTopPost();
+			return Ok(topPost);
+		}
 
-        [HttpGet("topVisit")]
-        public IActionResult topVisit()
-        {
-            var topVisit = _buyingAgentRepository.GetTopVisit();
-            return Ok(topVisit);
-        }
+		[HttpGet("topVisit")]
+		public IActionResult topVisit()
+		{
+			var topVisit = _buyingAgentRepository.GetTopVisit();
+			return Ok(topVisit);
+		}
 
-        [HttpGet("allProfit")]
-        public IActionResult allProfit()
-        {
-            var allProfit = _buyingAgentRepository.GetallProfit();
-            return Ok(allProfit);
-        }
+		[HttpGet("allProfit")]
+		public IActionResult allProfit()
+		{
+			var allProfit = _buyingAgentRepository.GetallProfit();
+			return Ok(allProfit);
+		}
 
-        [HttpGet("transactionsNum")]
-        public IActionResult transactionsNum()
-        {
-            return Ok(_buyingAgentRepository.GetTranactionsNum());
-        }
+		[HttpGet("transactionsNum")]
+		public IActionResult transactionsNum()
+		{
+			return Ok(_buyingAgentRepository.GetTranactionsNum());
+		}
 
-        [HttpGet("visitsNum")]
-        public IActionResult visitsNum()
-        {
-            return Ok(_buyingAgentRepository.GetVisitsNum());
-        }
-        [HttpGet("allTransactions")]
-        public IActionResult allTransactions()
-        {
-            var allTransactions = _buyingAgentRepository.GetAllTransactions();
-            return Ok(allTransactions);
-        }
+		[HttpGet("visitsNum")]
+		public IActionResult visitsNum()
+		{
+			return Ok(_buyingAgentRepository.GetVisitsNum());
+		}
+		[HttpGet("allTransactions")]
+		public IActionResult allTransactions()
+		{
+			var allTransactions = _buyingAgentRepository.GetAllTransactions();
+			return Ok(allTransactions);
+		}
 		[HttpGet("allVisits")]
 		public IActionResult allVisits()
 		{
