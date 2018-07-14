88c533d47fcbd3cb94f705dd4498908afb46d59f
wei zheng <tobe.zheng.wei@gmail.com>
2018-07-14 19:31:05 +1000

    add shopDto

---
+++ BuyingAgentBackEnd/Models/ShopDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuyingAgentBackEnd.Models
+{
+    public class ShopDto
+    {
+		[Required]
+		[MaxLength(20)]
+		public string Name { get; set; }
+		[MaxLength(100)]
+		public string Address { get; set; }
+		[MaxLength(50)]
+		public string ContactNo { get; set; }
+		[MaxLength(50)]
+		public string WeChatNo { get; set; }
+	}
+}
