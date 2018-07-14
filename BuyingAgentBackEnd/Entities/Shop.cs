47eb2a97ff4ef38c96a6c0300b334266208d642f
wei zheng <tobe.zheng.wei@gmail.com>
2018-07-14 19:30:25 +1000

    add shop entity

---
+++ BuyingAgentBackEnd/Entities/Shop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuyingAgentBackEnd.Entities
+{
+    public class Shop
+    {
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(20)]
+		public string Name { get; set; }
+
+		[MaxLength(100)]
+		public string Address { get; set; }
+
+		[MaxLength(50)]
+		public string ContactNo { get; set; }
+
+		[MaxLength(50)]
+		public string WeChatNo { get; set; }
+	}
+}
